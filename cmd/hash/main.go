e7e723c2c777a6f731b519551840fc42875d256f
theo <chippey.theo@gmail.com>
2025-06-03 14:22:44 +0200

    compte/page admin

--- cmd/hash/main.go
+++ cmd/hash/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
+	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
+	// Générer le hash du mot de passe "root"
 	password := []byte("root")
 	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("UPDATE users SET hashed_password = '%s' WHERE username = 'admin';\n", string(hashedPassword))
+
+	// Ouvrir la base de données
+	db, err := sql.Open("sqlite3", "data/tbcvclub.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Mettre à jour le mot de passe de l'admin
+	_, err = db.Exec("UPDATE users SET hashed_password = ? WHERE username = 'admin'", string(hashedPassword))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Le mot de passe de l'admin a été mis à jour avec succès !")
 }
